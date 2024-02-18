72e06b51e2db9d3956058719325a96ddca57d37e
Andy Cai <huayicai@gmail.com>
2024-02-18 16:37:23 +0800

    Add footer.html to login page

--- src/api/site/handler.rs
+++ src/api/site/handler.rs
@@ -20,6 +20,7 @@ struct Site {
     extra_js: String,
     reset_password_url: String,
     user_id_type: String,
+    copyright: String,
 }
 
 pub async fn login(engine: AppEngine) -> impl IntoResponse {
@@ -27,19 +28,20 @@ pub async fn login(engine: AppEngine) -> impl IntoResponse {
         signup_url:  "/auth/register".to_string(),
 		signup_text:  "Sign up".to_string(),
 		login_next:  "/admin".to_string(),
-		site_name:    "Weapi".to_string(),
+		site_name:    "Wepress".to_string(),
 		logo_url:    "/static/img/logo.svg".to_string(),
 		favicon_url: "/static/img/favicon.png".to_string(),
 		title:       "Sign in".to_string(),
         maincss_url: "".to_string(),
         description: "Wepress is a lightweight headless CMS".to_string(),
-        keywords: "cms, headless, wepress, weapi".to_string(),
+        keywords: "cms, headless, wepress, blog, website, admin".to_string(),
         extra_css: "".to_string(),
         extra_js: "".to_string(),
         reset_password_url: "/auth/reset".to_string(),
         user_id_type: "email".to_string(),
+        copyright: "2024 Wepress".to_string(),
     };
-    RenderHtml("login.html", engine, site)
+    RenderHtml("signin.html", engine, site)
 }
 
 pub async fn login_action() -> impl IntoResponse {
