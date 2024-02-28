9641063424779318fa8be74dafea2dc443b5072d
Andy Cai <huayicai@gmail.com>
2024-02-28 22:01:27 +0800

    Add middleware Cors

--- src/router.rs
+++ src/router.rs
@@ -2,7 +2,9 @@ use crate::admin::content::router as admin_router;
 use crate::api::content::router as api_router;
 use crate::api::site::router as site_router;
 
+use axum::http::{HeaderValue, Method};
 use axum::Router;
+use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
 
 pub fn routes() -> axum::Router {
@@ -10,5 +12,16 @@ pub fn routes() -> axum::Router {
         .nest("/", site_router::init_router())
         .nest("/admin", admin_router::init_router())
         .nest("/api", api_router::init_router())
+        .layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                .allow_methods([
+                    Method::GET,
+                    Method::POST,
+                    Method::PUT,
+                    Method::PATCH,
+                    Method::DELETE,
+                ]),
+        )
         .nest_service("/static", ServeDir::new("static"))
 }
